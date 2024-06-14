733554ec9af92f75a25ab97978e7e787d0338229
aounraza379 <aounmohsin1009@gmail.com>
2024-06-15 02:09:08 +0500

    Add files via upload
    ADDING FILE AS REPOSITORY

---
+++ myOOP.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+using namespace std;
+
+// class names{
+//     public:
+//     int a,b,c;
+//     void fnc() {
+//         cout << "a is: " << a << endl;
+//         cout << "b is: " << b << endl;
+//         cout << "c is: " << c << endl;
+//    	}
+// 	names(int one, int two, int three) {
+//     	a = one;
+//     	b = two;
+//     	c = three;
+//     }
+// };
+// int main() {
+//     names a1( 100, 200, 300 );
+//     a1.fnc();
+    
+//     names a2( 111, 222, 333 );
+//     a2.fnc();
+    
+//     return 0;
+// }
+
+// QUESTION 1
+// WRITE A CLASS THAT CONTAINS TWO INTEGERS DATA MEMBERS WHICH ARE INITIALIZED BY 50 AND 100 IN CONSTRUCTOR.
+// IT HAS MEMBER FUNCTION PRODUCT() THAT DISPLAYS THE PRODUCT OF DATA MEMBERS.
+
+// SOLUTION 1
+//class Data {
+//private:
+//    int n1;
+//    int n2;
+//    
+//public:
+//    Data() {
+//        n1 = 50;
+//        n2 = 100;
+//    }
+//    void product() {
+//        int result = n1 * n2;
+//        cout << "The Product is: " << result;
+//    }
+//};
+//
+//int main() {
+//    Data output;
+//    output.product();
+//    return 0;
+//}
+
+// QUESTION 2
+// DECLARE A CLASS NAME MARKS WITH 3 DATA MEMBERS TO STORE MARKS OF THREE SUBJECTS, MAKE FUNCTIONS IN() to input
+// details, SUM() to display sum of all marks, AVG() to calculate and return avg of all subjects.
+
+// SOLUTION 2
+//class Marks {
+//private:
+//    int marks1;
+//    int marks2;
+//    int marks3;
+//public:
+//    int input() {
+//        cout << "Enter marks in three subjects: \n"; 
+//        cin >> marks1 >> marks2 >> marks3;
+//        return 0;
+//    }
+//    int sum() {
+//        return marks1 + marks2 + marks3;
+//    }
+//    int avg() {
+//        return (marks1 + marks2 + marks3)/3;
+//    }
+//};
+//int main() {
+//    Marks show;
+//    show.input();
+//    int total = show.sum();
+//    int average = show.avg();
+//    cout << "Total marks: " << total << endl;
+//    cout << "Average marks: " << average << endl;
+//    return 0;
+//}
+
+// QUESTION 3
+// DECLARE CLASS NAME PLAYER WHICH HAS ATTRIBUTES PNAME, PAVG, PTEAM.MAKE TWO FUNCTIONS INPUT() for details AND
+// OUTPUT() to show details.KEEP RUNNING PROGRAM UNTIL USER PRESSING Y/y.
+
+// SOLUTION 3
+// class player
+
+
+
+
+
